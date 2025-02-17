49e1826a131a09ea72820a98e1efe38ad92be851
LOKESH DUMPALA <133016649+LOKESHDUMPALA@users.noreply.github.com>
2025-02-17 17:29:02 +0530

    Update UserProvider.jsx

--- frontend/src/Context/UserProvider.jsx
+++ frontend/src/Context/UserProvider.jsx
@@ -16,7 +16,7 @@ const UserProvider = ({children}) => {
               const teamname = user.teamname;
               // console.log(teamname);
               try{
-                const response = await axios.get(`https://teaminfo-9ygo.onrender.com/users/${teamname}`);
+                const response = await axios.get(`https://teaminfo-9ygo.onrender.com/api/user/users/${teamname}`);
                 const data = response.data;
                 setTeamData(data);
               } catch(error){
@@ -25,7 +25,7 @@ const UserProvider = ({children}) => {
             }
             else if(user && user.role==="Admin"){
               try{  
-                  const response =  await axios.get("https://teaminfo-9ygo.onrender.com/average_progress");
+                  const response =  await axios.get("https://teaminfo-9ygo.onrender.com/api/user/average_progress");
                   console.log(response.data);
                    const data = response.data;
                   setTeamProgress(data);
