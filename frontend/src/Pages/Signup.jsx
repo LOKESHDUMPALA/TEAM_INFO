799fcf05ef8cd1af6a0adbfac720c77dee925fa7
LOKESH DUMPALA <133016649+LOKESHDUMPALA@users.noreply.github.com>
2025-02-17 17:14:51 +0530

    Update Signup.jsx

--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -37,7 +37,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("https://teaminfo-9ygo.onrender.com/signup", formData, {
+      const response = await axios.post("https://teaminfo-9ygo.onrender.com/api/auth/signup", formData, {
         headers: {
           "Content-Type": "application/json",
         }
